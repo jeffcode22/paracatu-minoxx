abd32941a3a3299b9c0e196b252d83d2a3e6c176
jeffcode22 <jeff.ferna.code@gmail.com>
2022-11-16 15:14:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburguerIcon = document.querySelector(".hamburguer-icon")
+const menu = document.querySelector(".menu-container")
+
+hamburguerIcon.addEventListener("click", () =>{
+    menu.classList.toggle("ul-clicked")
+})
+
+console.log("ioide")
